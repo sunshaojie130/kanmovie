716bae50d2ab4d9fa06fb54c691ec633196f1257
你序言我深爱 <15383303557@163.com>
2019-11-25 16:50:08 +0800

    Every Components

--- README.md
+++ README.md
@@ -17,3 +17,25 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 脚手架的搭建
+```
+vue create kanmovie;
+Manually select features;
+``` 
+
+### git 远程仓库
+```
+git init 
+连接远程仓库
+git remote add origin https://github.com/sunshaojie130/kanmovie.git
+// 添加到本地仓库
+git add .
+// 上传到远程仓库
+git push origin master
+// 创建开发分支
+git checkout -b dev
+// 创建单独开发分支（方便开发，不容易起冲突）
+git checkout -b createComponents
+```
+
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main">
+        <Header title="看！我的"/>
+        <div id="content">
+            <Login />
+        </div>
+        <Tabbar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Tabbar from '@/components/Tabbar'
+import Login from '@/components/Login'
+export default {
+    name : 'Mine',
+    components: {
+        Header,
+        Tabbar,
+        Login
+    }
+}
+</script>
+
+<style>
+
+</style>
